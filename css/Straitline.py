5323dddeed323fa8b66a93edcebbe8ae90f1915e
SameerAhire21n78e <130359889+SameerAhire21n78e@users.noreply.github.com>
2024-02-09 12:47:42 +0530

    dance tour
    Using HTML, CSS, Bootstrap

---
+++ css/Straitline.py
@@ -0,0 +1,23 @@
+n = int(input(" Enter Any Two Points : ",x1,y1))
+
+print("Your Enter Points Are : ", x1, y1)
+
+n = int(input(" Enter Any Two Points : ",x2,y2))
+
+print("Your Enter Points Are : ", x2, y2)
+
+n = int(input(" Enter Any Two Points : ",x3,y3))
+
+print("Your Enter Points Are : ", x3, y3)
+
+m = (y2-y1)/(x2-x1)
+
+n = (y3-y2)/(x3-x2)
+
+if (m==n):
+
+    print("All Points Are In The Strait Line")
+
+else:
+
+    print("No Any Points Are In The Same Line ")
